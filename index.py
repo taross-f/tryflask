0148c271542cad1bbea713afb7d97f4f6a9aa87a
Taro Furuya <taro.furuya@gmail.com>
2017-06-24 15:42:53 +0900

    chap4

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', message='Hello')
+    return render_template('index.html')
 
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        msg = request.form['msg']
+        return render_template('index.html', message=msg)
+    else:
+        return redirect(url_for('index'))
 @app.route('/hello/')
 def hello():
     return render_template('hello.html', message='Hello world')
