aec76a9b8fa006de54d07ce340dac41dfca00972
Taro Furuya <taro.furuya@gmail.com>
2017-06-24 15:29:55 +0900

    chapter3

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', message='Hello')
+
+@app.route('/hello/')
+def hello():
+    return render_template('hello.html', message='Hello world')
 
 if __name__ == '__main__':
     app.debug = True
